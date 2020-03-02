caf0f43866d2edef54dd8d7e4acdffc2c462e211
Bryce Standley <bryce@vectorpixel.net>
2020-03-02 12:38:10 +0800

    Camera Look
    No player movement but the camera can now look around the scene with mouse input. Use esc to get your mouse back

---
+++ ICT371-Project/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float sensitivity = 100f; //Mouse input sensitivity
+
+    public Transform playerBody; //Reference to main player transform
+
+    private float xRot = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Hiding mouse and locking it to the center of the screen
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime; //Getting input from mouse for X and Y
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRot -= mouseY; //Reversing Y Input
+        xRot = Mathf.Clamp(xRot, -90f, 75f); // Clamping input to stop camera clipping
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f , 0f); //Applying Y Camera rotation
+        playerBody.Rotate(Vector3.up * mouseX); //Applying X camera rotation
+    }
+}
