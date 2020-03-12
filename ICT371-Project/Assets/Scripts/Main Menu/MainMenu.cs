d38af06e11c2a997cc0a6e1f90c3412ffbb6c591
Bryce Standley <bryce@vectorpixel.net>
2020-03-12 11:07:43 +0800

    0.01.1
    -Added basic main menu
    -Fixed camera sensitivity for player in test scene

---
+++ ICT371-Project/Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void GameInfo()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadTestLevel() // Needs to be removed for submission
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+}
