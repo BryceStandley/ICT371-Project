77f65586bbad67c07918d7f9cbf3231f82ba3a40
Bryce Standley <bryce@vectorpixel.net>
2020-03-26 09:29:33 +0800

    0.1.5
    -Added Tooltip for intractable items
    -Fixed velocity stacking for picked up items
    -Small Bug fixes with UI

--- ICT371-Project/Assets/Scripts/Objects/PickUp.cs
+++ ICT371-Project/Assets/Scripts/Objects/PickUp.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ObjectInformation))]
+[RequireComponent(typeof(Rigidbody))]
 public class PickUp : MonoBehaviour
 {
     public bool pickedUp = false;
 
     private GameObject holdPoint;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
 
     public void SetHoldPoint(GameObject point)
     {
         holdPoint = point;
+    }
+
+    public void DropItem()//setting fixed values to the rigidbody to stop it from gaining unwanted velocity, only do this once
+    {
+        rb.velocity = new Vector3(0, -9.5f * 0.7f, 0);
+        rb.angularVelocity = Vector3.zero;
+        rb.freezeRotation = false;
     }
 
     private void Update()
@@ -19,11 +35,8 @@ public class PickUp : MonoBehaviour
         {
             //constantly move towards a pickup point of the player
             transform.position = holdPoint.transform.position;
-            GetComponent<Rigidbody>().freezeRotation = true;
-        }
-        else
-        {
-            GetComponent<Rigidbody>().freezeRotation = false;
+            rb.freezeRotation = true;
+     
         }
     }
 }
