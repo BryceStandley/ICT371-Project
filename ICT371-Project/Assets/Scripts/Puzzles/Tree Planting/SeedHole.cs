ed5cbe84e12bd0e1c9d31e0ea17af5f0c64b1f97
Bryce Standley <bryce@vectorpixel.net>
2020-04-18 15:03:45 +0800

    0.2.3
    **Added** 
    Added more detail into the house in the backyard scene. Still mostly empty
    
    -Imported extra trees and vegetation packs (feel free to use the trees and extra vegetation to make the scene feel less empty)
    
    **Fixes**
    -Fixed a collision bug with picked up items with the house
    
    **Finished**
    -Dropping a seed into a hole now creates a tree in its place (more detail will be added soon)
    
    **Builds**
    -0.2.3 is available from the releases tab, built for windows, can build for macOS if required.

--- ICT371-Project/Assets/Scripts/Puzzles/Tree Planting/SeedHole.cs
+++ ICT371-Project/Assets/Scripts/Puzzles/Tree Planting/SeedHole.cs
@@ -4,12 +4,51 @@ using UnityEngine;
 
 public class SeedHole : MonoBehaviour
 {
+    private MeshFilter filter;
+    private MeshRenderer meshRenderer;
+    private MeshCollider meshCollider;
+    private GameObject parent;
+
+    public Mesh saplingMesh;
+    public Material saplingMaterial;
+
+    private void Awake()
+    {
+        parent = transform.parent.gameObject;
+        filter = parent.GetComponent<MeshFilter>();
+        meshRenderer = parent.GetComponent<MeshRenderer>();
+        meshCollider = parent.GetComponent<MeshCollider>();
+    }
+
     private void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.name == "Seed")
+        if (col.gameObject.name == "Seed")
         {
             Debug.Log("Seed was dropped into the hole!");
+            MoveSeed(col.gameObject);
+            PlantSapling();
+            this.gameObject.SetActive(false);
         }
-        Debug.Log("collision triggered with: " + col.gameObject);
+    }
+
+    private void MoveSeed(GameObject seed)
+    {
+        Rigidbody rb = seed.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.useGravity = false;
+        seed.transform.position = new Vector3(0, -100, 0);//Moving the seed below the game world and holding it in space
+
+    }
+
+    private void PlantSapling()// changing the mesh and materials of the hole object to the sapling object
+    {
+        filter.sharedMesh = saplingMesh;
+        meshRenderer.sharedMaterial = saplingMaterial;
+        float scale = Random.Range(0.3f, 0.8f);
+        parent.transform.localScale = new Vector3(scale, scale, scale);
+        parent.transform.Rotate(new Vector3(0, Random.Range(0, 360), 0));
+        meshCollider.sharedMesh = saplingMesh;
+
     }
 }
