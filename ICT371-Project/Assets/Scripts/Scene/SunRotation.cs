46988183901a9d4adf8385b2af243ef0eaddfc3a
Bryce Standley <bryce@vectorpixel.net>
2020-05-10 18:33:42 +0800

    0.3.11
    **Added**
    Added sun into the game that rotates over time

---
+++ ICT371-Project/Assets/Scripts/Scene/SunRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRotation : MonoBehaviour
+{
+    public float rotationTime = 0.01f;
+    private void LateUpdate() 
+    {
+        transform.RotateAround(transform.position, Vector3.left, rotationTime);
+    }
+}
